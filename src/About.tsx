95012f21185f8aa86af4b64a81abe31e1bb760f9
David <david@sirrus7.com>
2023-07-20 19:51:49 -0400

    text

--- src/About.tsx
+++ src/About.tsx
@@ -44,17 +44,17 @@ export function About() {
           A little about us...
         </Text>
         <Text size="sm" mt="sm" color="dimmed">
-          Welcome to the heartwarming story of the Columbia United Soccer Club, an extraordinary local amateur team hailing from the picturesque town of Columbia, Maryland. Brimming with camaraderie and a shared passion for soccer, this team embodies the true spirit of community.
+          Welcome to the heartwarming story of the Columbia Football Club(FC), an extraordinary local amateur team hailing from the picturesque town of Columbia, Maryland. Brimming with camaraderie and a shared passion for soccer, this team embodies the true spirit of community.
 
-          Founded on the belief that soccer can serve as a unifying force, Columbia United is not just a team; it's a family. Comprising players from diverse backgrounds, ages, and skill levels, they come together to promote the beautiful game and foster a sense of togetherness among their fellow Marylanders.
+          Founded on the belief that soccer can serve as a unifying force, Columbia FC is not just a team; it's a family. Comprising players from diverse backgrounds, ages, and skill levels, they come together to promote the beautiful game and foster a sense of togetherness among their fellow Marylanders.
 
           At the core of their mission is the desire to grow soccer throughout the entire state of Maryland. Their commitment to inclusivity means they actively reach out to underprivileged neighborhoods, organizing soccer clinics and outreach programs to encourage youngsters to embrace the sport they love.
 
-          But it's not just their community-oriented mindset that sets Columbia United apart; their dedication and talent on the field are equally remarkable. With a rich history of triumphant victories in various local and regional competitions, their achievements serve as a testament to the hard work, perseverance, and unwavering team spirit that fuels their success.
+          But it's not just their community-oriented mindset that sets Columbia FC apart; their dedication and talent on the field are equally remarkable. With a rich history of triumphant victories in various local and regional competitions, their achievements serve as a testament to the hard work, perseverance, and unwavering team spirit that fuels their success.
 
-          Yet, Columbia United remains humble and grounded, emphasizing sportsmanship, fair play, and respect for their opponents. They firmly believe that victory is even sweeter when accompanied by a shared sense of respect and admiration for the game and its players.
+          Yet, Columbia FC remains humble and grounded, emphasizing sportsmanship, fair play, and respect for their opponents. They firmly believe that victory is even sweeter when accompanied by a shared sense of respect and admiration for the game and its players.
 
-          As they continue their journey, Columbia United Soccer Club seeks to inspire the next generation of soccer enthusiasts, paving the way for a brighter future for the sport in Maryland. Their story is a reminder that when a community comes together, driven by passion and shared values, there are no limits to what they can achieve.
+          As they continue their journey, Columbia FC seeks to inspire the next generation of soccer enthusiasts, paving the way for a brighter future for the sport in Maryland. Their story is a reminder that when a community comes together, driven by passion and shared values, there are no limits to what they can achieve.
         </Text>
       </Paper>
   );
