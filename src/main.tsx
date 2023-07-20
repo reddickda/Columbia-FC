5626203683aae211742fe011c76ad0805c8c1231
David <david@sirrus7.com>
2023-07-20 18:56:41 -0400

    add routing

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,18 @@
 import ReactDOM from 'react-dom/client'
+import {
+  createBrowserRouter,
+  RouterProvider,
+} from "react-router-dom";
 import App from './App.tsx'
 import './index.css'
 
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App/>,
+  },
+]);
+
 ReactDOM.createRoot(document.getElementById('root')!).render(
-    <App />
+  <RouterProvider router={router} />
 )
